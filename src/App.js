d38f7b973db3e1f6e70c8fcdb586ce635c8070e9
Dharshan Muthukumar <149457260+Dharshan-c@users.noreply.github.com>
2024-08-16 11:32:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Navbar from "./Navbar";
+import Search from "./Search";
+import Product from "./Product";
+import Aboutus from "./Aboutus";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Search />
+      <Product />
+      <Aboutus />
+      <Footer />
+    </>
+  );
+}
+export default App;
